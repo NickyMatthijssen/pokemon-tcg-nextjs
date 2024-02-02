9fbc325f6d39815f530b9f4938f352eef73839ae
NickyMatthijssen <36468434+NickyMatthijssen@users.noreply.github.com>
2024-02-02 20:43:34 +0100

    No results and pagination skeleton

--- components/PokemonCardGrid.tsx
+++ components/PokemonCardGrid.tsx
@@ -9,6 +9,10 @@ export default async function PokemonCardGrid({
 }) {
   const { data, totalCount, pageSize, page } = await promise;
 
+  if (!data || data?.length < 1) {
+    return <NoResultsWarning />;
+  }
+
   return (
     <>
       <div className="grid sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-x-4">
@@ -24,5 +28,28 @@ export default async function PokemonCardGrid({
         prefix="/"
       />
     </>
+  );
+}
+
+function NoResultsWarning(): React.ReactElement {
+  return (
+    <div className="flex flex-col space-y-12 justify-center items-center h-[60vh]">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="w-44 h-44"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"
+        />
+      </svg>
+
+      <p className="text-xl">Sorry, no results found!</p>
+    </div>
   );
 }
--- components/PokemonCardGridSkeleton.tsx
+++ components/PokemonCardGridSkeleton.tsx
@@ -1,11 +1,22 @@
 import PokemonCardSkeleton from "./PokemonCardSkeleton";
+import Skeleton from "./Skeleton";
 
 export default function PokemonCardGridSkeleton() {
   return (
-    <div className="grid sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-x-4">
-      {Array.from(Array(12).keys()).map((_, i) => (
-        <PokemonCardSkeleton key={i} />
-      ))}
-    </div>
+    <>
+      <div className="grid sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-x-4">
+        {Array.from(Array(12).keys()).map((_, i) => (
+          <PokemonCardSkeleton key={i} />
+        ))}
+      </div>
+
+      <div className="flex items-center justify-between border-t border-neutral-700 mt-4 py-3">
+        <Skeleton height="20px" width="220px" className="hidden sm:block" />
+        <Skeleton height="36px" width="280px" className="hidden sm:block" />
+
+        <Skeleton height="36px" width="91.72px" className="sm:hidden" />
+        <Skeleton height="36px" width="65.43px" className="sm:hidden" />
+      </div>
+    </>
   );
 }
